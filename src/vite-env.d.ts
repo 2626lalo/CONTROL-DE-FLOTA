53269152d734d62d967e4cd557c45e306db300f2
2626lalo <alewilczek@gmail.com>
2025-12-15 16:51:42 -0300

    Create vite-env.d.ts

---
+++ src/vite-env.d.ts
@@ -0,0 +1,28 @@
+/// <reference types="vite/client" />
+
+declare module '*.svg' {
+  import React = require('react');
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement>
+  >;
+  const src: string;
+  export default src;
+}
+
+declare module '*.png' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.jpg' {
+  const content: string;
+  export default content;
+}
+
+interface ImportMetaEnv {
+  readonly VITE_GEMINI_API_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
