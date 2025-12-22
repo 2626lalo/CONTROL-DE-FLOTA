46e7a2906353972fe184aafc3c10f734837e118c
2626lalo <alewilczek@gmail.com>
2025-12-22 13:40:46 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   build: {
     outDir: 'dist',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
   },
-  base: '/', // Esto es importante para Render
-  server: {
-    historyApiFallback: true
-  }
+  publicDir: 'public' // ¡ESTO ES CRÍTICO!
 })
