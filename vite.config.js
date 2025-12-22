abf22883cb6fd5d4f1888d6525fe3c95ca2294db
2626lalo <alewilczek@gmail.com>
2025-12-22 13:36:52 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    target: 'es2020',
-    rollupOptions: {
-      external: ['@google/genai']
-    }
+    outDir: 'dist',
+    emptyOutDir: true
+  },
+  base: '/', // Esto es importante para Render
+  server: {
+    historyApiFallback: true
   }
 })
