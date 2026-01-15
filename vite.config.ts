ac773d0db6a688d29620235d87be83562a4d7c85
2626lalo <alewilczek@gmail.com>
2026-01-14 21:02:46 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,1 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    target: 'es2020',  // O 'es2019' para mayor compatibilidad
-    outDir: 'dist',
-    assetsDir: 'assets'
-  }
-})
