d69ec7b0e0470bc504e1693a34636cf9a27b528d
2626lalo <alewilczek@gmail.com>
2026-01-14 23:39:50 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,10 @@
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'  // Importa App.tsx de la misma carpeta
-import './index.css'     // Importa los estilos
+import { TestComponent } from './test'  // ← Importar componente de prueba
+import './index.css'
 
-// Esto es importante: encuentra el div con id="root"
-const rootElement = document.getElementById('root')
-
-// Si no encuentra el elemento, muestra error
-if (!rootElement) {
-  throw new Error('No se encontró el elemento con id="root" en index.html')
-}
-
-// Monta la aplicación React
-ReactDOM.createRoot(rootElement).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <TestComponent />
   </React.StrictMode>
 )
